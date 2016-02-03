de12499d069bba3696f7b4e38a173087d0e7f606
Mark Orel <mail.ormark@gmail.com>
2016-02-03 10:48:23 +0200

    Initial Commit

---
+++ swifttest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swifttest
+//
+//  Created by Mark Orel on 2/3/16.
+//  Copyright © 2016 Mark Orel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
